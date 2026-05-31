21316fdad44864c54f0bce245af6b13a8a7b481f
Anisha <anishagc83@gmail.com>
2026-05-31 22:44:57 +0545

    Navbar added in homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const PoppinsFont = Poppins({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${PoppinsFont.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,61 @@
-export default function Page() {
+import Image from "next/image";
+import logo from "@/app/assets/logo_1.png";
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
   return (
-    <div>This is home page</div>
+  <div>
+    <nav className="border-b bg-white">
+      <div className="container mx-auto flex h-15 items-center justify-between px-6">
+        
+        {/* Logo */}
+        <div>
+          <Image
+            src={logo}
+            alt="RentExpress Logo"
+            width={70}
+            height={50}
+            priority
+          />
+        </div>
+
+        {/* Navigation Links */}
+        <div className="flex items-center gap-10">
+          <a href="#" className="font-medium transition-colors duration-200 hover:text-cyan-500"
+          >Home
+          </a>
+
+          <a
+            href="#"
+            className="font-medium transition-colors duration-200 hover:text-cyan-500"
+          >Vehicle</a>
+
+          <a
+            href="#"
+            className="font-medium transition-colors duration-200 hover:text-cyan-500"
+          >Booking</a>
+
+          <a
+            href="#"
+            className="font-medium transition-colors duration-200 hover:text-cyan-500"
+          >category</a>
+
+           <a href="#"
+            className="font-medium transition-colors duration-200 hover:text-cyan-500"
+          >Favorite</a>
+        </div>
+
+        {/* Auth Buttons */}
+        <div className="flex items-center gap-3">
+          <Button variant="secondary">Login</Button>
+          <Button className="bg-cyan-500 hover:bg-cyan-600">
+            Register
+          </Button>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+   
   );
 }
