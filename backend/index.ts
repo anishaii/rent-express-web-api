928c7c5934dcf245923ea52fdb6d2a64958c4114
Anisha <anishagc83@gmail.com>
2026-06-06 23:06:57 +0545

    database connection setup with mongoose

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,8 @@
 import app from "./src/app";
 import { PORT } from "./src/configs/constant";
+import { connectToMongoDB } from "./src/database/mongodb";
+
+connectToMongoDB();
 
 app.listen(PORT, () => {
   console.log(`The Server is running on http://localhost:${PORT}`);
--- backend/src/configs/constant.ts
+++ backend/src/configs/constant.ts
@@ -3,3 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT: number = Number(process.env.PORT) || 5000;
+export const MONGODB_URL: string =
+  process.env.MONGODB_URL || "mongodb://localhost:27017/rentexpress-db";
