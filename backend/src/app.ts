1c6508faad69e159e104bb22d1028f8be8d7ddea
Anisha <anishagc83@gmail.com>
2026-06-07 14:34:13 +0545

    implementing user registration and login endpoints with password hashing and JWT generation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,28 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
+import { HttpException } from "./exceptions/http-exception";
+import { ApiResponseHelper } from "./utils/apihelper.util";
+import userRoutes from "./routes/user.route";
 
 const app: Application = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// user Route
+app.use("/api/auth", userRoutes);
+
+// global 404 handler (at bottom)
+app.use((req: Request, res: Response) => {
+  return res.status(404).json({ message: "API not found" });
+});
+
+// global error handler(at bottom)
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("Error:", err);
+  if (err instanceof HttpException) {
+    return ApiResponseHelper.error(res, err.message, err.status);
+  }
+  return ApiResponseHelper.error(res, "Internal Server Error", 500);
+});
 
 export default app;
