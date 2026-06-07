b8319a32ffdea3f2718623db6a549189435d80d8
Anisha <anishagc83@gmail.com>
2026-06-07 23:02:26 +0545

    save JWT and user data in cookies on login

--- frontend/src/lib/actions/auth-action.ts
+++ frontend/src/lib/actions/auth-action.ts
@@ -2,6 +2,8 @@
 
 import { RegisterFormData, LoginFormData } from "@/app/(auth)/_schema/schema";
 import { register, login } from "../api/auth";
+import { setTokenCookie, storeUserData } from "../cookies";
+
 // REGISTER
 export const handleRegisterUser = async (data: RegisterFormData) => {
   try {
@@ -23,6 +25,10 @@ export const handleRegisterUser = async (data: RegisterFormData) => {
 export const handleLoginUser = async (data: LoginFormData) => {
   try {
     const result = await login(data);
+    const user = result.data.user;
+    const token = result.data.token;
+    await setTokenCookie(token);
+    await storeUserData(user);
     if (result.success) {
       return { success: true, message: result.message, data: result.data };
     } else {
