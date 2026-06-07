cb809903ebf73140694d75cd677880c451da9dd8
Anisha <anishagc83@gmail.com>
2026-06-07 13:29:34 +0545

    jwt and bcrypt package installed

--- backend/src/configs/constant.ts
+++ backend/src/configs/constant.ts
@@ -5,3 +5,4 @@ dotenv.config();
 export const PORT: number = Number(process.env.PORT) || 5000;
 export const MONGODB_URL: string =
   process.env.MONGODB_URL || "mongodb://localhost:27017/rentexpress-db";
+export const SECRET_KEY: string = process.env.SECRET_KEY || "jwtsecretkey";
