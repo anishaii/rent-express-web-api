9beb050de0abbf851cd6bdf986b42025485672d4
Anisha <anishagc83@gmail.com>
2026-06-16 17:05:58 +0545

    add axios interceptor to auto-attach JWT token to requests

--- frontend/src/lib/api/axios-instance.ts
+++ frontend/src/lib/api/axios-instance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getTokenCookie } from "../cookies";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000";
 
@@ -8,5 +9,19 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// Automatically attach JWT token to every request
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const token = await getTokenCookie();
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
