e0d0b520d398045ad432035fe61068be6f839b85
Anisha <anishagc83@gmail.com>
2026-07-14 21:04:22 +0545

    add email.ts and update constant.ts

--- backend/src/configs/constant.ts
+++ backend/src/configs/constant.ts
@@ -6,3 +6,7 @@ export const PORT: number = Number(process.env.PORT) || 5000;
 export const MONGODB_URL: string =
   process.env.MONGODB_URL || "mongodb://localhost:27017/rentexpress-db";
 export const SECRET_KEY: string = process.env.SECRET_KEY || "jwtsecretkey";
+export const EMAIL_USER: string = process.env.EMAIL_USER || "";
+export const EMAIL_PASS: string = process.env.EMAIL_PASS || "";
+export const CLIENT_URL: string =
+  process.env.CLIENT_URL || "http://localhost:3000";
