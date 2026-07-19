cf17526d2097e3a7a7149de9a57a9483eb63ebe3
Anisha <anishagc83@gmail.com>
2026-07-19 15:42:59 +0545

    add folder str for testing

---
+++ frontend/e2e/test-data.ts
@@ -0,0 +1,11 @@
+export const testUser = {
+  fullName: "Test User",
+  email: "usere2e@gmail.com",
+  password: "123456",
+};
+
+export const testAdmin = {
+  fullName: "Admin User",
+  email: "admine2e@gmail.com",
+  password: "123456",
+};
